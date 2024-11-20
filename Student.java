567b6de043d803083be717852d569339dab585cc
anand <anandshukla8268@gmai.com>
2024-11-20 18:28:08 +0530

    Added Student code

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public void student() {
+        System.out.println("this is constructor");
+    }
+
+    public void sayHello() {
+        System.out.println("Hello from student");
+    }
+}
